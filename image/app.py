f87a306a226f24451007ee064583915f174663ee
Abdelmajid El-oualy <abdel-ou@c1r5p10.1337.ma>
2026-03-17 19:13:48 +0000

    change git actions

--- image/app.py
+++ image/app.py
@@ -10,7 +10,7 @@ async def read_root():
       <head>
         <style>
           h1 {
-            color: green;
+            color: #3498db;
             font-family: Arial, sans-serif;
             text-align: center;
             margin-top: 50px;

6c58025a4038c4c51882ae81c641cbbe39e4b595
Abdelmajid El-oualy <abdel-ou@c1r5p10.1337.ma>
2026-03-17 15:02:55 +0000

    change git actions

--- k8s/start_cluster.sh
+++ k8s/start_cluster.sh
@@ -6,4 +6,4 @@ kubectl apply -f service.yaml
 
 kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl port-forward svc/argocd-server -n argocd 8080:443

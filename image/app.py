f2644defe689e1a8d16b735ffc4b04fcba2587b7
Abdelmajid El-oualy <abdel-ou@c1r7p2.1337.ma>
2026-03-26 18:26:24 +0100

    repush

--- image/app.py
+++ image/app.py
@@ -1,29 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-
 app = FastAPI()
-
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return """
-    <html>
-      <head>
-        <style>
-          h1 {
-            color: green;
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-          }
-          body {
-            background: #f4f4f4;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Welcome this is version 1</h1>
-      </body>
-    </html>
-    """
+    return (
+        """
+        <html>
+          <head>
+            <style>
+              h1 {
+                color: green;
+                font-family: Arial, sans-serif;
+                text-align: center;
+                margin-top: 50px;
+              }
+              body {
+                background: #f4f4f4;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Welcome this is version 1</h1>
+          </body>
+        </html>
+        """
+    )
